7f0f81e7f63c90e1e3093ac4283932efddaadda9
Rakshith Mohan <rakshith21mohan@gmail.com>
2023-07-31 12:46:31 +0530

    corrections to euler tour

--- trees/euler_tour.cpp
+++ trees/euler_tour.cpp
@@ -6,18 +6,26 @@ vector<vector<int>> adj;
 vector<int> tin, tout;
 vector<int> euler;
 
-int t = 0;
+int t = -1;
 
 // dfs to build the euler tour of a tree
 void dfs(int i, int p){
     euler.emplace_back(i);
-    tin[i] = t++;
+    tin[i] = ++t;
     for(int j : adj[i]){
         if(j != p){
             dfs(j, i);
         }
     }
     tout[i] = t;
+}
+
+// CALL this method for buiding Euler tour
+void build_euler(int root, int n){
+    tin.resize(n);
+    tout.resize(n);
+    euler.clear();
+    dfs(root, -1);
 }
 
 int main(int argc, char const *argv[])
